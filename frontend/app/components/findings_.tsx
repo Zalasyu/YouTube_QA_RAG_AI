8ba54221dd42a26c2120ce8611e29c78be211804
LwaziNF <lwazinf@gmail.com>
2025-01-04 12:53:50 +0200

    Adding the chat interface

---
+++ frontend/app/components/findings_.tsx
@@ -0,0 +1,16 @@
+export const teams_ = () => {
+    return ( 
+        <div className={``}>
+            teams_
+        </div>
+     );
+}
+
+export const findings_ = () => {
+    return ( 
+        <div className={``}>
+            findings_
+        </div>
+     );
+}
+ 
