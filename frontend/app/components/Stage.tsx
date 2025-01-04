baf94a33f8010c0eb23ae14f9d854bc35e78cb87
LwaziNF <lwazinf@gmail.com>
2025-01-04 11:40:48 +0200

    Creating the basic layout structure.

--- frontend/app/components/Stage.tsx
+++ frontend/app/components/Stage.tsx
@@ -1,12 +1,26 @@
 "use client";
 
+import { useState } from 'react';
+
 const Stage = () => {
+  const [thumbnailExpanded, setThumbnailExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setThumbnailExpanded(!thumbnailExpanded);
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-row items-center justify-center">
       <div
-        className={`w-[500px] min-h-[100px] transition-all duration-200 rounded-[6px] p-1 flex-col justify-normal items-center bg-white shadow-md`}
+        onClick={toggleExpand}
+        className={`w-[500px] ${thumbnailExpanded ? 'h-[700px]' : 'h-[100px]'} mx-2 transition-all duration-200 rounded-[6px] p-1 flex-col justify-normal items-center bg-white shadow-md`}
       >
+      </div>
+      <div
+        onClick={toggleExpand}
+        className={`${thumbnailExpanded ? 'h-[700px] w-[500px]' : 'h-[0px] w-[0px]'} transition-all duration-200 rounded-[6px] p-1 flex-col justify-normal items-center`}
+      >
+        
       </div>
     </div>
   );
