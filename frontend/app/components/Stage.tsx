d9c7fe1930a5df94225790f42e25660a65f0752c
LwaziNF <lwazinf@gmail.com>
2025-01-04 11:55:27 +0200

    This works for now, we'll take care of small imperfections a bit later..

--- frontend/app/components/Stage.tsx
+++ frontend/app/components/Stage.tsx
@@ -10,17 +10,25 @@ const Stage = () => {
   };
 
   return (
-    <div className="flex flex-row items-center justify-center">
+    <div className="flex flex-row items-center justify-center scale-[.90]">
       <div
         onClick={toggleExpand}
         className={`w-[500px] ${thumbnailExpanded ? 'h-[700px]' : 'h-[100px]'} mx-2 transition-all duration-200 rounded-[6px] p-1 flex-col justify-normal items-center bg-white shadow-md`}
       >
       </div>
       <div
-        onClick={toggleExpand}
-        className={`${thumbnailExpanded ? 'h-[700px] w-[500px]' : 'h-[0px] w-[0px]'} transition-all duration-200 rounded-[6px] p-1 flex-col justify-normal items-center`}
+        className={`${thumbnailExpanded ? 'h-[700px] w-[500px]' : 'h-[0px] w-[0px]'} transition-all duration-200 rounded-[6px] flex-col justify-evenly items-center mb-3`}
       >
-        
+       {
+        [1, 2].map((obj_, idx_) => {
+          return (<div
+          key={idx_}
+            className={`${thumbnailExpanded ? 'w-full h-[49%]' : 'w-0 h-0'} transition-all duration-200 rounded-[6px] p-1 m-2 flex-col justify-normal items-center bg-white shadow-md`}
+          >
+            
+          </div>)
+        })
+       } 
       </div>
     </div>
   );
